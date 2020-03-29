bbdc80a9f48464fe06bba8c4cf8bba10179a51a8
Barry Low <bearcanrun@gmail.com>
2020-03-29 00:29:21 -0700

    fix(designspek-components): add colspan to TD + pub 0.3.6

--- libs/components/src/components/Table/TD.svelte
+++ libs/components/src/components/Table/TD.svelte
@@ -4,12 +4,12 @@
   export let style = $$props.style || {}
   export let theme = $$props.theme || style.theme || {}
   export let ssr = $$props.ssr || style.ssr || false
-
+  export let colspan = 1
   $: compStyles = styled(style, theme, ssr)
   $: styleProps = ssr ? { style: compStyles } : { class: compStyles }
   // $: console.log('Box', style, theme, compStyles, styleProps)
 </script>
 
-<td {...styleProps}>
+<td {colspan} {...styleProps}>
   <slot />
 </td>
