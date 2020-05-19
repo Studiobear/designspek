c859e70f02beabae3421980959b98d5d4c5b17da
Barry Low <bearcanrun@gmail.com>
2020-05-18 21:49:29 -0700

    feat(designspek): transform, perspective, transition
    short codes + pass through

--- libs/core/src/index.js
+++ libs/core/src/index.js
@@ -165,6 +165,17 @@ const forwardStyleDefault = [
   'verticalAlign',
   'transition',
   'transform',
+  'transformStyle',
+  'transformOrigin',
+  'perspective',
+  'perspective-origin',
+  'tranf',
+  'tranfor',
+  'tranit',
+  'transt',
+  'per',
+  'peror',
+  'bck',
 ]
 
 let styleLib = {}
