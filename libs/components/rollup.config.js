2fa3d2e313a64defd3c73abb51166143ac120ea3
Barry Low <bearcanrun@gmail.com>
2020-09-17 18:00:59 -0700

    build(components): reconfigure rollup + build

--- libs/components/rollup.config.js
+++ libs/components/rollup.config.js
@@ -1,9 +1,12 @@
 import svelte from 'rollup-plugin-svelte'
-import sveltePreprocess from 'svelte-preprocess';
+import autoPreprocess from 'svelte-preprocess';
 import typescript from '@rollup/plugin-typescript';
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
+import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json'
+
+const production = process.env.NODE_ENV
 
 const name = pkg.name
   .replace(/^(@\S+\/)?(svelte-)?(\S+)/, '$3')
@@ -18,20 +21,31 @@ export default {
       file: pkg.module,
       format: 'es',
       globals: { '@studiobear/designspek': 'svelteSystemUi' },
+      sourcemap: production,
     },
     {
       file: pkg.main,
       format: 'umd',
       name,
       globals: { '@studiobear/designspek': 'svelteSystemUi' },
+      sourcemap: production,
     },
   ],
   plugins: [
-    svelte({
-      preprocess: sveltePreprocess(),
-    }),
-    resolve(),
+    commonjs(),
     typescript({ sourceMap: !production }),
-    commonjs()
+    svelte({
+      dev: !production,
+      preprocess: autoPreprocess(),
+    }),
+    resolve({
+      dedupe: [
+        'svelte',
+      ],
+    }),
+    production && terser(),
   ],
+  watch: {
+    clearScreen: false,
+  },
 }
