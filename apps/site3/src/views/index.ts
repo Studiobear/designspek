f456022e6dbc54223e00451694e9256626fbb2b1
Barry Low <bearcanrun@gmail.com>
2020-09-25 13:42:07 -0700

    fix(site3): remove Dashboard.svelte

--- apps/site3/src/views/index.ts
+++ apps/site3/src/views/index.ts
@@ -1,5 +1,4 @@
-import Dashboard from './Dashboard.svelte'
 import Landing from './Landing.svelte'
 import NotFound from './NotFound.svelte'
 
-export { Dashboard, Landing, NotFound }
+export { Landing, NotFound }
